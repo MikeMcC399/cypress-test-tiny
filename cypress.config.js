3b775ca31549df504ec0797445a2d75036246154
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2022-12-02 20:27:20 +0100

    Add customizations

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,9 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  video: false,
+  fixturesFolder: false,
   e2e: {
-    setupNodeEvents(on, config) {},
+    supportFile: false,
   },
-}
+});
