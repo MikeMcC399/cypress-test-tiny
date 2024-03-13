80782e1aa94e86e8c60a8dcf28eeb10bfdac91b5
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2024-03-13 07:07:02 +0100

    Install Cypress with examples

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,10 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {},
+    supportFile: false,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
-}
+});
