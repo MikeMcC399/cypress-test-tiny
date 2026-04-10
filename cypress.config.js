86600cbf2747148dcca2234f3854f1f7f73caef7
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2026-04-10 16:57:35 +0200

    Scaffold single cookies test

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,9 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  allowCypressEnv: false,
+  fixturesFolder: false,
   e2e: {
-    setupNodeEvents(on, config) {},
+    supportFile: false,
   },
-}
+});
